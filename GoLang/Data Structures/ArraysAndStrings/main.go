d49bce697232ce8af5378f22ad96e5f33023cdc3
Connor Burk <connor.burk@bayer.com>
2020-10-27 20:15:23 -0500

    isUniqueV2()

--- GoLang/Data Structures/ArraysAndStrings/main.go
+++ GoLang/Data Structures/ArraysAndStrings/main.go
@@ -17,10 +17,23 @@ func isUnique(str string) bool {
 	return true
 }
 
+func isUniqueV2(str string) bool {
+	store := 0
+
+	for _, char := range(str) {
+		value := char - 'a'
+		if ((store & (1 << value)) > 0) {
+			return false
+		}
+		store |= (1 << value)
+	}
+	return true
+}
+
 func main() {
 	uniqueString := "abcdef"
 	nonUniqueString := "abcdaef"
 
-	fmt.Printf("%v is a unique string (%v)\n", uniqueString, isUnique(uniqueString))
-	fmt.Printf("%v is a unique string (%v)\n", nonUniqueString, isUnique(nonUniqueString))
+	fmt.Printf("%v is a unique string (%v)\n", uniqueString, isUniqueV2(uniqueString))
+	fmt.Printf("%v is a unique string (%v)\n", nonUniqueString, isUniqueV2(nonUniqueString))
 }
