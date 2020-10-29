c2637b27f52691edf11a62e9d61596111e4e11f9
Connor Burk <connor.burk@bayer.com>
2020-10-28 19:17:50 -0500

    MergeSort round 2 - Dax & Seth Rogen

--- GoLang/Algorithms/MergeSort/main.go
+++ GoLang/Algorithms/MergeSort/main.go
@@ -37,10 +37,45 @@ func MergeSort(arr []int) []int {
 	mid := len(arr) / 2
 
 	return merge(MergeSort(arr[0:mid]), MergeSort(arr[mid:]))
-} 
+}
+
+func merge2(left []int, right []int) []int {
+	l := 0
+	r := 0
+	sorted := []int{}
+
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			sorted = append(sorted, left[l])
+			l++
+		} else {
+			sorted = append(sorted, right[r])
+			r++
+		}
+	}
+	for l < len(left) {
+		sorted = append(sorted, left[l])
+		l++
+	}
+	for r < len(right) {
+		sorted = append(sorted, right[r])
+		r++
+	}
+	return sorted
+}
+
+func MergeSort2(arr []int) []int {
+	if len(arr) == 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+
+	return merge2(MergeSort2(arr[0:mid]), MergeSort2(arr[mid:]))
+}
 
 func main() {
 	arr := []int{54, 24, 23, 32, 1, 36, 3, 17, 52}
 
-	fmt.Println(MergeSort(arr))
+	fmt.Println(MergeSort2(arr))
 }
