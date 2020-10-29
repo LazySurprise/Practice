6c947608053b63ec74721693b057d59f98e1acf0
Connor Burk <connor.burk@bayer.com>
2020-10-28 19:03:43 -0500

    check if 2 strings are palindromes

--- GoLang/Data Structures/ArraysAndStrings/main.go
+++ GoLang/Data Structures/ArraysAndStrings/main.go
@@ -29,10 +29,34 @@ func isUniqueV2(str string) bool {
 	return true
 }
 
+func isPalindrome(s1 string, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	chars := make(map[rune]int)
+	for _, char := range(s1) {
+		chars[char]++
+	}
+	for _, char := range(s2) {
+		chars[char]--
+		if chars[char] < 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	uniqueString := "abcdef"
 	nonUniqueString := "abcdaef"
 
 	fmt.Printf("%v is a unique string (%v)\n", uniqueString, isUniqueV2(uniqueString))
 	fmt.Printf("%v is a unique string (%v)\n", nonUniqueString, isUniqueV2(nonUniqueString))
+
+	s1 := "abcd"
+	s2 := "abdc"
+	s3 := "abcc"
+
+	fmt.Printf("%v is a palindrome of %v (%v)\n", s1, s2, isPalindrome(s1, s2))
+	fmt.Printf("%v is a palindrome of %v (%v)\n", s2, s3, isPalindrome(s2, s3))
 }
