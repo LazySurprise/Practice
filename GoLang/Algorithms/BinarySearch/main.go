3de0f76d697834c2390e025ab4f5097218cf488f
Connor Burk <connor.burk@bayer.com>
2020-10-28 19:08:57 -0500

    Binary Search Round 2 - Halloween 2020

--- GoLang/Algorithms/BinarySearch/main.go
+++ GoLang/Algorithms/BinarySearch/main.go
@@ -23,8 +23,27 @@ func BinarySearch(arr []int, key int) bool {
 	return false
 }
 
+func BinarySearch2(arr []int, key int) bool {
+	l := 0
+	r := len(arr) - 1
+	m := (l + r) / 2
+
+	for l <= r {
+		if key < arr[m] {
+			r = m - 1
+			m = (l + r) / 2
+		} else if key > arr[m] {
+			l = m + 1
+			m = (l + r) / 2
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	fmt.Println(BinarySearch(arr, 7))
+	fmt.Println(BinarySearch2(arr, 4))
 }
